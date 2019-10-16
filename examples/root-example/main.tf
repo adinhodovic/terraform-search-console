271b49520c3ee7611e92c206dd933295ff8855db
Adin Hodovic <hodovicadin@gmail.com>
2019-10-16 22:16:42 +0200

    Use zone_id

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -1,9 +1,10 @@
 module "example_com_search_console" {
-  source = "github.com/adinhodovic/terraform-search-console"
+  source = "github.com/adinhodovic/terraform-txt-verification"
 
   cloudflare_zones = [
     {
-      "zone" : "example.com",
+      "zone_id" : "213123",
+      "name" : "@",
       "site_verification" : "google-site-verification=14P5CXSZMJhOqwk12lW9CZojRiak2Y8X0nRz7WQ46IKG43E44"
     }
   ]
--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
 resource "cloudflare_record" "default" {
-  count  = length(var.cloudflare_zones)
-  domain = lookup(var.cloudflare_zones[count.index], "zone")
-  name   = "@"
-  value  = lookup(var.cloudflare_zones[count.index], "site_verification")
-  type   = "TXT"
+  count   = length(var.cloudflare_zones)
+  zone_id = lookup(var.cloudflare_zones[count.index], "zone_id")
+  name    = lookup(var.aws_route53_domains[count.index], "name")
+  value   = lookup(var.cloudflare_zones[count.index], "site_verification")
+  type    = "TXT"
 }
 
 resource "aws_route53_record" "default" {
   count   = length(var.aws_route53_domains)
   zone_id = lookup(var.aws_route53_domains[count.index], "zone_id")
-  name    = lookup(var.aws_route53_domains[count.index], "domain")
-  type    = "TXT"
-  ttl     = "300"
+  name    = lookup(var.aws_route53_domains[count.index], "name")
   records = lookup(var.aws_route53_domains[count.index], "site_verification")
+  type    = "TXT"
 }
